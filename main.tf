487b0fd53b86fda7afa825277d73ce555143a087
ArielLeza <ariel.leza@gmail.com>
2021-07-21 13:31:59 +0300

    bla

--- main.tf
+++ main.tf
@@ -17,19 +17,6 @@ resource "aci_vrf" "vrf1" {
   name      = "VRF-Prod"
 }
 
-resource "aci_bridge_domain" "bd1" {
-  tenant_dn                = aci_tenant.demo.id
-  relation_fv_rs_ctx       = aci_vrf.vrf1.id
-  relation_fv_rs_bd_to_out = [data.aci_l3_outside.internet.id]
-  name                     = "bd1"
-}
-resource "aci_bridge_domain" "bd2" {
-  tenant_dn                = aci_tenant.demo.id
-  relation_fv_rs_ctx       = aci_vrf.vrf1.id
-  relation_fv_rs_bd_to_out = [data.aci_l3_outside.internet.id]
-  name                     = "bd2"
-}
-
 resource "aci_subnet" "bd1_subnet" {
   parent_dn = aci_bridge_domain.bd1.id
   ip        = var.bd1Subnet
