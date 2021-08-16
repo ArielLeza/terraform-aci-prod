3b9b463e969239598f0c95a468818a507931b6ed
ArielLeza <ariel.leza@gmail.com>
2021-08-16 12:36:11 +0300

    removed epg

--- Application.tf
+++ Application.tf
@@ -9,14 +9,3 @@ resource "aci_application_epg" "epg1" {
   relation_fv_rs_bd      = aci_bridge_domain.bd1.id
   #relation_fv_rs_cons    = [aci_contract.contract_epg1_epg2.id, data.aci_contract.default.id]
 }
-
-resource "aci_epg_to_domain" "vmmepg1" {
-
-  application_epg_dn    = aci_application_epg.epg1.id
-  tdn                   = data.aci_vmm_domain.vds.id
-  vmm_allow_promiscuous = "accept"
-  vmm_forged_transmits  = "accept"
-  vmm_mac_changes       = "accept"
-  instr_imedcy          = "immediate"
-  res_imedcy            = "pre-provision"
-}
