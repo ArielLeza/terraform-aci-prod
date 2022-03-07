eeec8fb3fb92e0a66997798ab0f1c2fda107be7d
ArielLeza <ariel.leza@gmail.com>
2022-03-07 13:21:29 +0200

    t

--- versions.tf
+++ versions.tf
@@ -1,9 +1,22 @@
+#terraform {
+#  required_version = ">= 0.13"
+#  required_providers {
+#    aci = {
+#      source = "ciscodevnet/aci"
+#      version = "0.7.1"
+#    }
+#  }
+#}
+
 terraform {
-  required_version = ">= 0.13"
   required_providers {
     aci = {
-      source = "ciscodevnet/aci"
-      version = "0.7.1"
+      source = "CiscoDevNet/aci"
+      version = "2.0.1"
     }
   }
+}
+
+provider "aci" {
+  # Configuration options
 }
